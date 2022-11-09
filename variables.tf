d2db294a7983353d559d8bc3cafe9fc417888881
root <root@ip-172-31-42-27.ap-southeast-1.compute.internal>
2022-11-09 16:37:19 +0000

    project

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  type =string
+  default = "ap-southeast-1"
+  
+}
+
+variable "subnet_id_1" {
+
+  type = string
+
+  default = "subnet-045758b19ff301ca4"
+
+ }
+ variable "subnet_id_2" {
+
+  type = string
+
+  default = "subnet-081593132768b353b"
+
+ }
+ variable "subnet_id_3" {
+  
+  type = string
+
+  default = "subnet-0119e2e1e4b024330"
+
+ }
+
+ variable "vpc_id" {
+  type = string
+  default = "vpc-0c043fd9556420c12"
+   
+ }
